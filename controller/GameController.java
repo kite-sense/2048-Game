5a3ef3b22a3a17742df717c5ba9f8b03f5b10e6e
kite-sense <160223226+kite-sense@users.noreply.github.com>
2024-05-13 11:16:56 +0800

    Add files via upload

--- controller/GameController.java
+++ controller/GameController.java
@@ -1,13 +1,18 @@
 package controller;
 
 import model.GridNumber;
+import util.AdvancedFileUtil;
 import view.GamePanel;
+
+import javax.swing.*;
+import java.util.List;
 
 
 /**
  * This class is used for interactive with JButton in GameFrame.
  */
 public class GameController {
+    public static AdvancedFileUtil advancedFileUtil = new AdvancedFileUtil();
     private GamePanel view;
     private GridNumber model;
 
@@ -17,8 +22,24 @@ public class GameController {
         this.model = model;
 
     }
+
     public void restartGame() {
         System.out.println("Do restart game here");
+        model.firstInitialNumbers();
+        view.updateGridsNumber();
+        view.clearStep();
+    }
+
+    public void saveGame(String path) {
+        List<String> lines = model.convertToList();
+        lines.add(Integer.toString(view.getSteps()));
+        advancedFileUtil.writeFileFromList(path, lines);
+    }
+
+    public int loadGame(String path) {
+        List<String> lines = advancedFileUtil.convertFileToList(path);
+        int step = model.convertToChessboard(lines);
+        return step;
     }
 
     //todo: add other methods such as loadGame, saveGame...
